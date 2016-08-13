de80d1dc6173ca7b74b868731427928a27dda5ca
Midi <midi13@163.com>
2016-08-13 06:46:12 +0000

    add flash message for visitors controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,5 +2,7 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
     render 'visitors/new'
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
   end
 end
